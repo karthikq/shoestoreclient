8b54fd3dad92e0f9cf3df7e0ab9b9fc1c07c5620
karthikq <kk968346@gmail.com>
2022-05-26 20:30:07 +0530

    se22

--- src/components/UserIp/Getuserip.js
+++ src/components/UserIp/Getuserip.js
@@ -2,7 +2,7 @@
 
 const axios = require("axios");
 // https://api.ipdata.co?api-key=${"0b953632748eeb22974bdb44dda243b5fa8cc01c4d2dd55f7c2223d0"}
-exports.getUserip = async () => {
+export const getUserip = async () => {
   try {
     const data = await axios.get("https://geolocation-db.com/json/");
     if (data.data) {
