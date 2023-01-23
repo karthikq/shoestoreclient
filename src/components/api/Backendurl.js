09c8f3b5bf799217f38aae15d6af19f77f407e1f
karthikq <kk968346@gmail.com>
2023-01-23 20:41:42 +0530

    backendUrl

--- src/components/api/Backendurl.js
+++ src/components/api/Backendurl.js
@@ -2,7 +2,7 @@
 
 export const backendUrl = () => {
   if (process.env.NODE_ENV === "production") {
-    return "https://shoe-store-app.onrender.com/v1";
+    return "https://shoestoreserver-production.up.railway.app/v1";
   } else {
     return "http://localhost:5000/v1";
   }
